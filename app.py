21dcdd845641270654482c1e30a9c8ce154c4e20
Blax Fontaine <valerie.gehle@epitech.eu>
2020-05-18 22:17:50 +0200

    display numbers on click

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import parser
 from tkinter import *
 
 # create basic window
@@ -5,7 +6,7 @@ window = Tk()
 
 # window params
 window.title("Kalculator")
-window.geometry("400x600")
+window.geometry("1000x468+0+0")
 
 # set app icon
 icon = PhotoImage(file="calc.gif")
@@ -15,29 +16,79 @@ window.iconphoto(True, icon)
 window.config(background='#1E1F21')
 
 # content
-label_title = Label(window, text="Welcome to Kalculator", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-label_title.pack()
 
-one_button = Button(window, text="1", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-one_button.pack()
-two_button = Button(window, text="2", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-two_button.pack()
-three_button = Button(window, text="3", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-three_button.pack()
-four_button = Button(window, text="4", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-four_button.pack()
-five_button = Button(window, text="5", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-five_button.pack()
-six_button = Button(window, text="6", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-six_button.pack()
-seven_button = Button(window, text="7", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-seven_button.pack()
-eight_button = Button(window, text="8", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-eight_button.pack()
-nine_button = Button(window, text="9", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-nine_button.pack()
-zero_button = Button(window, text="0", font=("Roboto", 20), bg='#1E1F21', fg='#CCA871')
-zero_button.pack()
+calc = Frame(window, bg='#1E1F21')
+calc.grid()
+
+# label_title = Label(calc, text="Welcome to Kalculator", font=("Courier", 20), bg='#1E1F21', fg='#CCA871')
+# label_title.grid()
+
+txtDisplay = Entry(calc, font=("Courier", 20, "bold"), bg='#1E1F21', fg='#CCA871', bd=20, width=28, justify=RIGHT)
+txtDisplay.grid(row=0, column=0, columnspan=9, pady=1)
+txtDisplay.insert(0, "0")
+
+def button_click(num):
+    # num = int(num)
+    txtDisplay.delete(0, END)
+    txtDisplay.insert(0, num)
+
+
+numberpad = "789456123"
+i = 0
+btn = []
+
+for j in range(2, 5):
+    for k in range(3):
+        btn.append(Button(calc, width=6, height=2, font=("Courier", 20, "bold"), bg='#1E1F21', fg='#CCA871', bd=1, text=numberpad[i]))
+        btn[i].grid(row=j, column= k, pady=1)
+        btn[i]["command"]=lambda x=numberpad[i]: button_click(x)
+        i+=1
+
+btnClear = Button(calc, text="C", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnClear.grid(row=1, column=0, pady=1)
+
+btnAllClear = Button(calc, text="CE", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnAllClear.grid(row=1, column=1, pady=1)
+
+btnSqrRoot = Button(calc, text="√", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnSqrRoot.grid(row=1, column=2, pady=1)
+
+btnAdd = Button(calc, text="+", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnAdd.grid(row=1, column=3, pady=1)
+
+btnMinus = Button(calc, text="-", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnMinus.grid(row=2, column=3, pady=1)
+
+btnTimes = Button(calc, text="*", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnTimes.grid(row=3, column=3, pady=1)
+
+btnDivide = Button(calc, text="÷", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnDivide.grid(row=4, column=3, pady=1)
+
+btnZero = Button(calc, text="0", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnZero.grid(row=5, column=0, pady=1)
+
+btnDot = Button(calc, text=".", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnDot.grid(row=5, column=1, pady=1)
+
+btnSqr = Button(calc, text="²", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnSqr.grid(row=5, column=3, pady=1)
+
+btnEq = Button(calc, text="=", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#CCA871', fg='#1E1F21')
+btnEq.grid(row=5, column=2, pady=1)
+
+btnPi = Button(calc, text="π", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnPi.grid(row=1, column=4, pady=1)
+
+btnSin = Button(calc, text="Sin", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnSin.grid(row=1, column=5, pady=1)
+
+btnCos = Button(calc, text="Cos", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnCos.grid(row=1, column=6, pady=1)
+
+btnTan = Button(calc, text="Tan", width=6, height=2, font=("Courier", 20, "bold"), bd=1, bg='#1E1F21', fg='#CCA871')
+btnTan.grid(row=1, column=7, pady=1)
+
 
 # display window
 window.mainloop()
